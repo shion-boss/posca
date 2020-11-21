22f47053a3c2a65367b87a43843b243aa41adeab
shion-boss <sss.tl.ges.sss@gmail.com>
2020-11-21 19:57:24 +0900

    a

--- igposca/views.py
+++ igposca/views.py
@@ -10,29 +10,52 @@ from django.conf import settings
 
 
 def index_view(request):
-    #options = webdriver.ChromeOptions()
-    #options.add_argument('--headless')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
     #※headlessにしている
-    options = Options()
+    #options = Options()
     #headlessの設定をTrueにする
-    options.headless = True
+    #options.headless = True
     driver = webdriver.Chrome(options=options)
     #driver = webdriver.Chrome()
     driver.implicitly_wait(60)
     driver.get('https://www.instagram.com/?hl=ja')
+    time.sleep(1)
     l=driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
     l.send_keys('dn.2a1')
+    time.sleep(1)
     r=driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
     r.send_keys(settings.PAS)
+    time.sleep(1)
     driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]').click()
+    time.sleep(1)
     driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
-    driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[4]/a').click()
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div[2]/a').click()
-    a=driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/h2')
-    t=a.get_attribute("textContent")
+    time.sleep(1)
+    try:
+        driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+    except:
+        pass
+    time.sleep(1)
+    driver.get('https://www.instagram.com/explore/tags/娘/')
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div/div[2]').click()
+    time.sleep(1)
+    e=driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[2]/div/div/div[2]')
+    webdriver.ActionChains(driver).double_click(e).perform()
+    time.sleep(1)
+    driver.find_element_by_xpath('/html/body/div[5]/div[1]/div/div/a').click()
+    for i in range(10):
+        e=driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[2]/div/div/div[2]')
+        webdriver.ActionChains(driver).double_click(e).perform()
+        time.sleep(1)
+        driver.find_element_by_xpath('/html/body/div[5]/div[1]/div/div/a[2]').click()
+
+
+
+
     params={
-        'a':t,
+        'a':'1',
     }
     return render(request,'igposca/index.html',params)
 
@@ -46,34 +69,44 @@ def test_ajax_response(request):
     if request.method == 'POST':
         input_text = request.POST['input_data']
         hoge = "Ajax Response: " + str(input_text)
-        #options = webdriver.ChromeOptions()
+        options = webdriver.ChromeOptions()
         #options.add_argument('--headless')
-        options = Options()
+        #options = Options()
         #headlessの設定をTrueにする
-        options.headless = True
+        #options.headless = True
         #※headlessにしている
+        options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome(options=options)
         #driver = webdriver.Chrome()
         driver.implicitly_wait(60)
         driver.get('https://www.instagram.com/?hl=ja')
+        time.sleep(1)
         l=driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
         l.send_keys('dn.2a1')
+        time.sleep(1)
         r=driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
         r.send_keys(settings.PAS)
+        time.sleep(1)
         driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]').click()
+        time.sleep(1)
         driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+        time.sleep(1)
         driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+        time.sleep(1)
         driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[4]/a').click()
+        time.sleep(1)
         driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div[2]/a').click()
-        a=driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/h2')
-        t=a.get_attribute("textContent")
+        time.sleep(1)
+        a=driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/h2').get_attribute("textContent")
+        time.sleep(1)
 
         #search_box = driver.find_element_by_name("q")
         #search_box.send_keys('インスタグラム')
         #search_box.submit()
         #s=driver.find_element_by_xpath('//*[@id="rso"]/div['+str(input_text)+']/div/div[1]/a/h3/span').click()
         #a=driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[2]/div/p/a/span').get_attribute("textContent")
-        driver.quit()
-        return HttpResponse(t)
+        #driver.quit()
+        #driver.close()
+        return HttpResponse(a)
     else:
         return HttpResponse('こんにちわ')
