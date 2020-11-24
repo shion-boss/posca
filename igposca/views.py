b051c8cc567880b702413bd5fc36c73d3aa8abcb
shion-boss <sss.tl.ges.sss@gmail.com>
2020-11-25 06:53:06 +0900

    Update views.py

--- igposca/views.py
+++ igposca/views.py
@@ -171,6 +171,7 @@ def test_ajax_response(request):
 def search_tags_ajax_view(request):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(options=options)
