b08efbd22f6b51d3a8c2638ee1b86437e8bb748d
shion-boss <sss.tl.ges.sss@gmail.com>
2020-11-28 06:13:47 +0900

    Update celery.py

--- posca/celery.py
+++ posca/celery.py
@@ -14,8 +14,8 @@ app = Celery('posca')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
-#app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+#app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task(bind=True)
 def debug_task(self):
