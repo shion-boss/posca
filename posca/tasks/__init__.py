d1e7ad05ab6e7be11b6c917e9ad6b7539aa6c357
shion-boss <sss.tl.ges.sss@gmail.com>
2020-11-27 22:24:46 +0900

    c

--- posca/tasks/__init__.py
+++ posca/tasks/__init__.py
@@ -1,9 +1,9 @@
 from ..celery import app
 
-
 @app.task()
 def add_numbers(a, b):
     print('Request: {}'.format(a + b))
+    print('いい感じだよ！おまえは天才')
     return a + b
 
 #from posca.tasks import add_numbers
