de2776f3b98ce63834d618ad352bb34640770323
shion-boss <sss.tl.ges.sss@gmail.com>
2020-11-28 00:46:47 +0900

    Update settings.py

--- posca/settings.py
+++ posca/settings.py
@@ -281,8 +281,8 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab(minute=30, hour=21),
     }
 }
-CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
-CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+CELERY_BROKER_URL = os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
