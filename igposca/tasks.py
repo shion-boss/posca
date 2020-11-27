a0e425046df171e85f4d6061a7d712ae0cd18eef
shion-boss <sss.tl.ges.sss@gmail.com>
2020-11-28 01:55:54 +0900

    Update tasks.py

--- igposca/tasks.py
+++ igposca/tasks.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 from celery import task
 from . import models
+from django.http import HttpResponse
 
 @task(name='task_number_one')
 def some_task():
     models.taged_data(igname='test',top_img_url='a',main_img_url='b',text='',page_url='https://abe').save()
-    return print('お前は天才')
+    return HttpResponse('tasks完了です')
