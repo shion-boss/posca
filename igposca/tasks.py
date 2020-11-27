7089a696e5375f0a9a6c3a1ec0a421e93095e5e6
shion-boss <sss.tl.ges.sss@gmail.com>
2020-11-28 01:05:39 +0900

    Update tasks.py

--- igposca/tasks.py
+++ igposca/tasks.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 from celery import task
 from . import models
 
-@task(name='task-number-one')
+@task(name='task_number_one')
 def some_task():
     models.taged_data(igname='test',top_img_url='a',main_img_url='b',text='',page_url='https://abe').save()
     return print('お前は天才')
