3d8bf6dbff9e74883db40b06fd37862e1411c9d7
shion-boss <sss.tl.ges.sss@gmail.com>
2020-11-28 04:44:27 +0900

    l

--- igposca/tasks.py
+++ igposca/tasks.py
@@ -14,3 +14,33 @@ def add_numbers():
     taged_data(igname='test',top_img_url='a',main_img_url='b',text='',page_url='https://abe').save()
     message='tasks2完了です'
     return HttpResponse(message)
+
+from celery import shared_task
+from .models import Widget
+
+
+@shared_task
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
+
+
+@shared_task
+def count_widgets():
+    return Widget.objects.count()
+
+
+@shared_task
+def rename_widget(widget_id, name):
+    w = Widget.objects.get(id=widget_id)
+    w.name = name
+    w.save()
