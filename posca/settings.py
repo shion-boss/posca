b4800c30675c9addcb355c8bea3c792759aa4c7b
shion-boss <sss.tl.ges.sss@gmail.com>
2020-12-09 20:33:38 +0900

    Update settings.py

--- posca/settings.py
+++ posca/settings.py
@@ -280,14 +280,17 @@ if not DEBUG:
             'task': 'task_search_taged',
             'schedule': crontab(minute=0, hour=12),
         },
-        'task-likes': {
-            'task': 'task_likes',
-            'schedule': timedelta(minutes=60),
-        },
+
     }
     #'args': (10, 15),
     #'schedule': timedelta(minutes=24*60),
     #'schedule': crontab(minute=30, hour=21),
+    """
+    'task-likes': {
+        'task': 'task_likes',
+        'schedule': timedelta(minutes=60),
+    },
+    """
     CELERY_ALWAYS_EAGER = False
     CELERY_BROKER_URL = os.environ.get('REDIS_URL')
     CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
